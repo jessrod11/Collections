914b3a37624b0d3633f77e3dcd182308abf312a3
Stix <jessica.rodgers@gmail.com>
2018-09-08 13:07:41 -0500

    added code

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Collections
 {
@@ -6,7 +7,67 @@ namespace Collections
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var array = new string[5];
+
+            var three = 3.ToString();
+
+            /*if (true)
+                DoSomething();*/
+                
+            //Same as below
+
+           /* if (true)
+            {
+                DoSomething();
+            }*/
+
+            // Collections - Lists
+
+            var list = new List<string>
+            {
+                "Martin",
+                "Ben",
+                three
+            };
+
+            list.Add("Nathan");
+
+            Predicate<string> temp = x => x == "Martin";
+
+            var martinExists = list.Exists(temp);
+
+            // var dictionary = new Dictionary<int, string>();
+
+            // Collections - Dictionary
+
+            var spouses = new Dictionary<MiniFigure, MiniFigure>();
+
+            var todd = new MiniFigure();
+            var toddSpouse = new MiniFigure();
+            spouses.Add(todd, toddSpouse);
+            // this example "just performed a wedding lol"
+            var spouseKeys = spouses.Keys;
+
+            // var toddRealSpouse; must be implicitly typed or initialized with a value
+
+            //MiniFigure toddRealSpouse; same as what is listed below. it is being declared outside in this case.
+        
+            spouses.TryGetValue(todd, out  MiniFigure toddRealSpouse);
+
+            // Collections - Tuple 
+            var tuple = new Tuple< string, int, MiniFigure>("Martin", 42, todd);
+
+            var name = tuple.Item1;
         }
+
+        static void DoSomething()
+        {
+            Console.WriteLine("Something");
+        } 
+
+        public class MiniFigure
+        {
+
+        };
     }
 }
